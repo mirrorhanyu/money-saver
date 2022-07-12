dc653820db06927645e410476ff7caa8a197276d
mirrorhanyu <mirrorhanyu@gmail.com>
2022-07-12 21:42:44 +0800

    add WeRoBot

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.9
+
+MAINTAINER Han Yu mirrorhanyu@gmail.com
+
+COPY . /app
--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import os
 from decimal import Decimal
 
+import werobot
+
 from helper.tbk import request
-
-query = "【老爸抽检】英氏婴幼儿维C加铁营养米粉辅食宝宝1段高铁米糊258g"
-
-material_response = request('taobao.tbk.dg.material.optional', {
-    "adzone_id": os.getenv('TBK_ADZONE_ID'),
-    "q": query
-}).json()
-
-result = next(result for result in material_response['result_list'] if result['title'] == query)
 
 # url = "https:" + material_response['result_list'][0]['url']
 # num_iid = material_response['result_list'][0]['num_iid']
-
-print("money get", Decimal(result['zk_final_price']) * Decimal(result['commission_rate']) / Decimal(10000))
 
 # tkl_response = request('taobao.tbk.tpwd.create', {
 #     "url": url,
@@ -23,3 +14,26 @@ print("money get", Decimal(result['zk_final_price']) * Decimal(result['commissio
 # }).json()
 #
 # print(tkl_response.json()['data']['password_simple'])
+
+robot = werobot.WeRoBot(token=os.getenv('WECHAT_OA_TOKEN'))
+
+
+@robot.handler
+def hello(message):
+    print("I receive message", message)
+    query = "【老爸抽检】英氏婴幼儿维C加铁营养米粉辅食宝宝1段高铁米糊258g"
+    material_response = request('taobao.tbk.dg.material.optional', {
+        "adzone_id": os.getenv('TBK_ADZONE_ID'),
+        "q": query
+    }).json()
+    result = next(result for result in material_response['result_list'] if result['title'] == query)
+    money = Decimal(result['zk_final_price']) * Decimal(result['commission_rate']) / Decimal(10000)
+    print("money get", money)
+    return str(money)
+
+
+robot.config['HOST'] = '0.0.0.0'
+robot.config['PORT'] = 80
+robot.config["APP_ID"] = os.getenv('WECHAT_OA_APP_ID')
+robot.config['ENCODING_AES_KEY'] = os.getenv('WECHAT_OA_AES')
+robot.run()
