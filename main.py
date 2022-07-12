641956927a8a26ab39cd09148d990b3a4812a5f5
mirrorhanyu <mirrorhanyu@gmail.com>
2022-07-12 12:35:05 +0800

    return fanli money

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
+from decimal import Decimal
 
 from helper.tbk import request
 
+query = "【老爸抽检】英氏婴幼儿维C加铁营养米粉辅食宝宝1段高铁米糊258g"
+
 material_response = request('taobao.tbk.dg.material.optional', {
     "adzone_id": os.getenv('TBK_ADZONE_ID'),
-    "q": "【老爸抽检】英氏婴幼儿维C加铁营养米粉辅食宝宝1段高铁米糊258g"
-})
+    "q": query
+}).json()
 
-url = "https:" + material_response.json()['result_list'][0]['url']
+result = next(result for result in material_response['result_list'] if result['title'] == query)
 
-tkl_response = request('taobao.tbk.tpwd.create', {
-    "url": url,
-    "sub_pid": os.getenv('TBK_SUB_PID'),
-})
+# url = "https:" + material_response['result_list'][0]['url']
+# num_iid = material_response['result_list'][0]['num_iid']
 
-print(tkl_response.json()['data']['password_simple'])
+print("money get", Decimal(result['zk_final_price']) * Decimal(result['commission_rate']) / Decimal(10000))
+
+# tkl_response = request('taobao.tbk.tpwd.create', {
+#     "url": url,
+#     "sub_pid": os.getenv('TBK_SUB_PID'),
+# }).json()
+#
+# print(tkl_response.json()['data']['password_simple'])
