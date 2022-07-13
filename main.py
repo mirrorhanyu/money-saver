186e9570725d619f77e9ac684a240497d70457cc
mirrorhanyu <mirrorhanyu@gmail.com>
2022-07-13 12:43:31 +0800

    add logging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from decimal import Decimal
 
 import werobot
+from werobot.logger import logger
 
 from helper.tbk import request
 
@@ -31,8 +32,7 @@ def subscribe(message):
 
 @robot.text
 def hello(message):
-    print("I receive text", message)
-    logging.info(message)
+    logger.info(message)
     query = "【老爸抽检】英氏婴幼儿维C加铁营养米粉辅食宝宝1段高铁米糊258g"
     material_response = request('taobao.tbk.dg.material.optional', {
         "adzone_id": os.getenv('TBK_ADZONE_ID'),
